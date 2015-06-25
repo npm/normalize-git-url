1cca46f605d5266e75cb51e050c0f2b716542130
Kat Marchán <kzm@sykosomatic.org>
2015-06-24 17:12:41 -0700

    standardising

--- normalize-git-url.js
+++ normalize-git-url.js
@@ -1,4 +1,4 @@
-var url = require("url")
+var url = require('url')
 
 module.exports = function normalize (u) {
   var parsed = url.parse(u, true)
@@ -13,8 +13,8 @@ module.exports = function normalize (u) {
   }
 
   // figure out what we should check out.
-  var checkout = parsed.hash && parsed.hash.substr(1) || "master"
-  parsed.hash = ""
+  var checkout = parsed.hash && parsed.hash.substr(1) || 'master'
+  parsed.hash = ''
 
   var returnedUrl
   if (parsed.pathname.match(/\/?:/)) {
